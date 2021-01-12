e5e4acc98ce7316ebcb55a9389eea83fe0e4d2ef
Edem Dorfe <dayonafi@gmail.com>
2021-01-12 11:24:33 +0900

    Priority

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -70,6 +70,6 @@ class TasksController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def task_params
-      params.require(:task).permit(:title, :content, :id, :tasks, :sort, :deadline, :status)
+      params.require(:task).permit(:title, :content, :id, :tasks, :sort, :deadline, :status, :priority)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_08_085012) do
+ActiveRecord::Schema.define(version: 2021_01_12_021521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_08_085012) do
     t.datetime "updated_at", null: false
     t.date "deadline"
     t.string "status"
+    t.string "priority"
   end
 
 end
